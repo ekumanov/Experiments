dec53e8b9f3e4013013b32ddef992a771209c4af
Evgeni Kumanov <kumanov@gmail.com>
2022-10-08 15:44:34 +0300

    Use min percentage for admission, sort results

--- src/PartySeats.java
+++ src/PartySeats.java
@@ -1,33 +1,40 @@
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class PartySeats {
     public static final int TOTAL_SEATS = 240;
+    public static final double MIN_PERCENTAGE = 4.0;
     public static Map<String, Double> results = new LinkedHashMap<>();
 
     static {
-        results.put("ГЕРБ", 25.2);
-        results.put("ПП", 16.5);
-        results.put("ДПС", 13.1);
-        results.put("Възраждане", 11.0);
-        results.put("БСП", 10.2);
-        results.put("ДБ", 8.4);
-        results.put("БВ", 4.4);
-        results.put("ИТН", 4.0);
+        results.put("ГЕРБ", 25.33);
+        results.put("ПП", 20.20);
+        results.put("ДПС", 13.75);
+        results.put("Възраждане", 10.18);
+        results.put("БСП", 9.30);
+        results.put("ДБ", 7.45);
+        results.put("БВ", 4.63);
+        results.put("ИТН", 3.83);
     }
 
     public static void main(String[] args) {
-        double totalPercentage = results.values().stream().mapToDouble(d -> d).sum();
+        List<String> filtered = new LinkedList<>(results.keySet());
+        filtered.removeIf(s -> results.get(s) < MIN_PERCENTAGE);
+        filtered.sort((party1, party2) -> (int) (results.get(party2) - results.get(party1)));
+
+        double totalPercentage = filtered.stream().map(s -> results.get(s)).mapToDouble(d -> d).sum();
         double factor = TOTAL_SEATS / totalPercentage;
         int usedSeats = 0;
         Map<String, Double> remainders = new LinkedHashMap<>();
         Map<String, Integer> partySeats = new LinkedHashMap<>();
-        for (Map.Entry<String, Double> partyResult : results.entrySet()) {
-            double scaled = partyResult.getValue() * factor;
+        for (String party : filtered) {
+            double scaled = results.get(party) * factor;
             int seats = (int) Math.floor(scaled);
-            partySeats.put(partyResult.getKey(), seats);
+            partySeats.put(party, seats);
             double remainder = scaled - seats;
-            remainders.put(partyResult.getKey(), remainder);
+            remainders.put(party, remainder);
             usedSeats += seats;
         }
         System.out.println("First round seats: " + usedSeats);
